c13405e9bfaf831242468fa94e8e4628633241eb
BenediktJSchlegel <benediktjschlegel@gmail.com>
2021-03-27 15:43:00 +0100

    AWP Block5 + Hausaufgabe HappyBike

---
+++ BikeThing/BikeThing.cpp
@@ -0,0 +1,83 @@
+
+#include <iostream>
+
+using namespace std;
+
+double calculate_distance(int x_pos, int y_pos, int x_dest, int y_dest);
+int get_random_position();
+void init();
+
+const int bike_count = 50;
+
+int ids[bike_count];
+int x_positions[bike_count];
+int y_positions[bike_count];
+
+int main()
+{
+	init();
+
+	while (true) {
+
+		int x_input;
+		int y_input;
+
+		cout << "Bitte geben Sie Ihre X Position ein: ";
+		cin >> x_input;
+		cout << "Bitte geben Sie Ihre Y Position ein: ";
+		cin >> y_input;
+
+		cout << endl << endl << "Fahrrad wird gesucht . . ." << endl << endl;
+
+		int best_index;
+		// = 2000 = Terrible workaround for first iteration
+		double best_distance = 2000;
+
+		for (int i = 0; i < bike_count; i++)
+		{
+			double distance = calculate_distance(x_input, y_input, x_positions[i], y_positions[i]);
+
+			if (distance < best_distance) {
+				best_index = i;
+				best_distance = distance;
+
+				cout << "Besseres Rad gefunden: " << "ID: " << ids[best_index] << ", Distanz: " << best_distance << " m" << endl;
+			}
+		}
+
+		cout << endl << endl;
+		cout << "Bitte buchen Sie das Rad mit der ID: " << ids[best_index] << " in " << best_distance << " Metern Entfernung.";
+		cout << endl << endl;
+	}
+}
+
+/// <summary>
+/// Generate values for Arrays
+/// ID = index + 1. 1 - 50
+/// x/y pos = 0 - 1000 Random int
+/// </summary>
+void init() {
+	//Seed the (semi) random number generator => Different values each run
+	srand(time(NULL));
+
+	for (int i = 0; i < bike_count; i++)
+	{
+		ids[i] = i + 1;
+		x_positions[i] = get_random_position();
+		y_positions[i] = get_random_position();
+	}
+}
+
+int get_random_position() {
+	//Rnd in range 0 - 1000
+	return rand() % 1000 + 1;
+}
+
+double calculate_distance(int x_pos, int y_pos, int x_dest, int y_dest)
+{
+	int x_delta = x_pos - x_dest;
+	int y_delta = y_pos - y_dest;
+
+	double distance = sqrt((x_delta * x_delta) + (y_delta * y_delta));
+	return distance;
+}
---
+++ CSProject/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace CSProject
+{
+    class Program
+    {
+        private static int _bikeCount = 50;
+        private static BikeList _bikes;
+
+        static void Main(string[] args)
+        {
+            Init();
+            RunBikeInput();
+        }
+
+        private static void RunBikeInput()
+        {
+            while (true)
+            {
+                Position position = GetPositionInput();
+                Bike closestBike = _bikes.GetClosestBikeTo(position);
+
+                Console.WriteLine(String.Format("Bitte buchen Sie das Rad mit der ID: {0} in {1} Metern Entfernung", closestBike.Id, closestBike.BikePosition.DistanceTo(position).ToString("0.##")));
+                Console.WriteLine(Environment.NewLine + Environment.NewLine);
+            }
+        }
+
+        private static Position GetPositionInput()
+        {
+            try
+            {
+                Console.WriteLine("Bitte geben Sie Ihre X Position ein");
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Bitte geben Sie Ihre Y Position ein");
+                int y = Convert.ToInt32(Console.ReadLine());
+
+                return new Position(x, y);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Positionen können nur durch Zahlen angegeben werden");
+                Console.WriteLine(Environment.NewLine);
+                //Bad Input => Rerun recursively
+                return GetPositionInput();
+            }
+
+        }
+
+        private static void Init()
+        {
+            _bikes = new BikeList();
+
+            for (int i = 0; i < _bikeCount; i++)
+            {
+                _bikes.Add(new Bike(i + 1));
+            }
+        }
+    }
+
+    public class BikeList : List<Bike>
+    {
+        public Bike GetClosestBikeTo(Position position)
+        {
+            //Can't use Linq because of required Bike by Bike output
+            Bike closestBike = this[0];
+            double closestDistance = closestBike.BikePosition.DistanceTo(position);
+
+            foreach (Bike bike in this)
+            {
+                double currentDistance = bike.BikePosition.DistanceTo(position);
+
+                if (currentDistance < closestDistance)
+                {
+                    closestBike = bike;
+                    closestDistance = currentDistance;
+                    Console.WriteLine("Besseres Rad gefunden: ID = " + bike.Id + " Distanz = " + currentDistance.ToString("0.##"));
+                }
+            }
+
+            Console.WriteLine(Environment.NewLine);
+
+            return closestBike;
+        }
+    }
+
+    public class Bike
+    {
+        public int Id { get; private set; }
+        public Position BikePosition { get; private set; }
+
+        public Bike(int id)
+        {
+            this.Id = id;
+            this.BikePosition = new Position();
+        }
+
+    }
+
+    public class Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        /// <summary>
+        /// Random Constructor => Generates Random position
+        /// </summary>
+        public Position()
+        {
+            var rnd = new Random();
+            X = rnd.Next(0, 1000);
+            Y = rnd.Next(0, 1000);
+        }
+
+        public Position(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public double DistanceTo(Position pos)
+        {
+            int xDelta = pos.X - this.X;
+            int yDelta = pos.Y - this.Y;
+            
+            double distance = Math.Sqrt(Math.Pow(xDelta, 2) + Math.Pow(yDelta, 2));
+            return distance;
+        }
+    }
+
+
+}
